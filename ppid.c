a7aad26360e761e20d185ac36ef0bfb12ede6c42
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-18 21:32:55 +0000

    created ppid file, same as pid structure - Ariel

---
+++ ppid.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ *main - entry point
+ *@void: no arguments
+ *Return:
+**/
+int main (void)
+{
+        ppid_t ppid;/** **/
+
+        ppid getppid();/** **/
+
+        /**print ppid to ensure its working also use echo$$**/
+
+        return ();/** **/
+}
