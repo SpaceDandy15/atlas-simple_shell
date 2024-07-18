ae8ba0ad4ca0cbe2947ca7f6735b5b8fdf5df5ce
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-18 21:31:39 +0000

    created pid file - Ariel

---
+++ pid.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ *main - entry point
+ *@void: no arguments
+ *Return:
+**/
+int main (void)
+{
+	pid_t pid;/** **/
+
+	pid getpid();/** **/
+
+	/**print pid to ensure its working also use echo$$**/
+
+	return ();/** **/
+}
