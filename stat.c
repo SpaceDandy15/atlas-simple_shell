cd2358e87d1a044c9a8b4e8bae982514423a3519
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-18 21:41:16 +0000

    created stat file - Ariel

---
+++ stat.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ *main - entry point
+ *@ac: 
+ *@av:
+ *@env
+ *Returns 0 in success, -1 if unsuccessful
+**/
+int main (int ac, char **av, **env)
+{
+
+}
