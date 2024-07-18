a9f29241ce21a99235babcdfae484477857a82a7
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-18 21:27:24 +0000

    created fork file - Ariel

---
+++ fork.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ *main - entry point
+ *@void: no arguments
+ *Returns 0 in the child, -1 if unsuccessful
+**/
+int main (void)
+{
+
+}
