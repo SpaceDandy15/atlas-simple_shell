53c6c9bb0e66260e0fb020728a57c40466be8fda
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-20 22:56:14 +0000

    grabbed functions from man pages to header, will need to redo for readability - Ariel

--- shell.h
+++ shell.h
@@ -18,7 +18,15 @@ int main (void);
 int main (int ac, char **av);
 int main (int ac, char **av, **env);
 char *strtok(char *str, const char *delim);
-pid_t getppid(void);
-ppid_t getppid(void);
+pid_t getppid(void);/**reference in man page**/
+ppid_t getppid(void);/**same as above**/
+pid_t fork(void);/**fork man page**/
+int stat(const char *restrict pathname, struct stat *restrict statbuf);
+pid_t wait(int *_Nullable wstatus);
+pid_t waitpid(pid_t pid, int *_Nullable wstatus, int options);
+int execve(const char *pathname, char *const _Nullable argv[], char *const _Nullable envp[]);
+int isatty(int fd);
+ssize_t getline(char **restrict lineptr, size_t *restrict n, FILE *restrict stream);
+
 
 #endif
