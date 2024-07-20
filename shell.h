889efce73caeb7470faff8d864f211131b3e2e91
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-20 21:01:40 +0000

    additional code in header, inc functions, library, macro and possible struct - Ariel

--- shell.h
+++ shell.h
@@ -1,8 +1,22 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
+#define BUFFER
+#define
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
 
+/**typedef struct{
+	
+	
+};**/
+
+int main (void);
+int main (int ac, char **av);
+int main (int ac, char **av, **env);
+char *strtok(char *str, const char *delim);
+
+#endif
