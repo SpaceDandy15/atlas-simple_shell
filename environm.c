e199e328f9d7bf68dc10fa0dd80c36f8cc0c7dc4
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-25 17:15:28 -0500

    changes in environment file. adding path -ARIEL

--- environm.c
+++ environm.c
@@ -9,16 +9,18 @@
  *@argv: array including file name
  *Return:-1 if an error
 **/
-int execve(const char *pathname, char *const argv[])
+int execve(const char *pathname, char *const argv[], char *const env[])
 {
-	const char *path;
-	char *const argv [] = {"/usr/bin/ls", "ls", "NULL"};
+	const char *path = "/bin/sh";
+	char *const argv[] = {"/bin/sh", NULL};
+	char *const env[] = {NULL};
 
 	if (execve(cmd, argv[0], argv) == -1)/**handling errors**/
 {
 		printf("Error\n");
 		return (-1);/**returns error**/
 }
+	fork();
 	isatty();/**will recurse and handle and fork**/
 	printf("");/**prints if execve funct is successful**/
 }
@@ -29,15 +31,16 @@ int execve(const char *pathname, char *const argv[])
 **/
 int isatty(int fd);/**don't need this will look into working it into code wednesday**/
 {
-	if (isatty() == 1)/**checks if terminal**/
+	if (fd == 1)/**checks if terminal**/
 {
-		fork();/**will call create second pathe for execve to follow**/
+		getline();/**will recurse prompt */
+		fork();/**will call create second path for execve to follow**/
 		printf("Terminal acquired, Major.\n");/**GiTS reference**/
-		return ();/**returns terminal**/
+		return (1);/**returns terminal**/
 }
 	else
 {
 		printf("Failed to acquire terminal, Major!\n");/**is not a terminal**/
 }
-	return ();
+	return (0);
 }
