7f7922f9f7938dc1d072bdbb8ac4969c6118778d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-27 19:45:43 -0500

    removed env file and added isatty func to main, still need recurse for prompt - Ariel

--- main.c
+++ main.c
@@ -3,7 +3,17 @@
 /**maybe recurse everything in here**/
 int main(void)
 {
-	while (1)
+	if (isatty(fd) == 1)/**checks if terminal**/
+{
+		printf("Terminal acquired, Major.\n");/**GiTS reference**/
+		return (1);/**returns terminal**/
+}
+	else
+{
+		printf("Failed to acquire terminal, Major!\n");/**is not a terminal**/
+}
+	return (0);
+
 	{
 		prompt();
 	}
