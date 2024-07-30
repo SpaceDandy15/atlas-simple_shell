ec410fbf74398ca523160270ee489872f81b14cc
Malik Vance <malik@atlassian.com>
2024-07-29 21:30:51 -0500

    fixing exec.c

--- exec.c
+++ exec.c
@@ -58,68 +58,20 @@ int _path(char **input, char **env, int *ex_st)
  * Return: void
 */
 
-void _custom_exec(const char *command, char *const args[], int *exit_status)
+void _custom_exec(const char *command, char *const args[], int *exit_status) 
 {
-	int i; /** integer used as loop counter*/
-	char *temp, *left, *right; /** pointer used for string manipulation*/
-	char *new = NULL, *envcopy = NULL; /** temporary storage for manipulated paths , envcopy is a copy fo the current environment variable being processed.*/
-	pid_t pid = fork();
+    pid_t pid = fork();
 
-	for (i = 0; env[i] != NULL; i++) /** loop iterates over each environment variable until it encounters a null terminator ('\0'),
-	indicates the end of the environment variables array.*/
-	{
-		envcopy = _strdup(env[i]);
-		left = strtok(envcopy, "= \t");
-		temp = trtok(NULL, "=\t"); /** environment is duplicated using _stdup, the split into tokens based on spaces,
-		equals signs, and tabs. First token is stored in left and second token is in temp*/
-
-		if (_strcmp(left, "PATH") == 0) /** checks if left token matches the string "PATH", if it does it will proceed to tokenized temp again, this time splitting
-		on colons, spaces, and tabs to get individual paths.*/
-		{
-			right = strtok(temp, ": \t");
-			while (right)
-			{
-				new = pathstr(right);
-
-				if (access(new, X_OK) == 0)
-				{
-					if (pid == 0)
-					_custom_exec(new,input, NULL);
-
-					else if (pid > 0)
-					{
-					wait (NULL);
-					*ex_st = 0;
-					}
-					else
-					{
-						perror("fork");
-					}
-					free(new);
-					free(envcopy);
-					return (0);
-				}
-				right = strtok(NULL, ": \t");
-				free(new); /** for each path that is found, pathstr will manipulate it. This also checks
-				if the modified path is executable using access and if it is FORKS a new process to execute 
-				the command using exec. After execution child process finishes and sets the exit status to 0.*/
-			}
-		}
-		free(envcopy);
-	}
-	else if (pid < 0) /**fork failed*/
-	{
-		perror("Failed to fork");
-		exit(EXIT_FAILURE);
-	}
-	else /** parent process waits for child*/
-	{
-		int status;
-		waitpid(pid, &status, 0);
-		*exit_status = WEXITSTATUS(status);/**wait for child to terminate */
-	}
+    if (pid == 0) { /**Child process*/
+        if (execvp(command, args) == -1) {
+            perror("Failed to execute command");
+            exit(EXIT_FAILURE);
+        }
+    } else if (pid > 0) { /** Parent process*/
+        wait(NULL); /** Wait for the child process to finish*/
+        *exit_status = WEXITSTATUS(errno); // Set the exit status
+    } else { /** Fork failed*/
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    }
 }
-	return (2); /** after processing all paths, the function frees memory alocated for envcopy and returns 2,
-	 indicating failure to find a valid executable path*/
-}
-
